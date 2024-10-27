eb76a84e0b0e73279bdec85bbd965198b62059e4
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-10-27 18:17:52 +0530

    added main 3d model to Hero.jsx

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,77 @@
+import {Canvas} from "@react-three/fiber";
+import {PerspectiveCamera} from "@react-three/drei";
+import HackerRoom from "../components/HackerRoom.jsx";
+import {Suspense} from "react";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import {Leva, useControls} from "leva";
+import {max} from "three/src/nodes/math/MathNode.js";
+
+
+const Hero = () => {
+    const x= useControls
+    ('HackerRoom',{
+        positionX: {
+            value:2.5,
+            min:-10,
+            max:10,
+        },
+        positionY: {
+            value:2.5,
+            min:-10,
+            max:10,
+        },
+        positionZ: {
+            value:2.5,
+            min:-10,
+            max:10,
+        },
+        rotationX: {
+            value:2.5,
+            min:-10,
+            max:10,
+        },
+        rotationY: {
+            value:2.5,
+            min:-10,
+            max:10,
+        },
+        rotationZ: {
+            value:2.5,
+            min:-10,
+            max:10,
+        },
+        scale: {
+            value:2.5,
+            min:-10,
+            max:10,
+        }
+    })
+    return (
+        <section className="min-h-screen w-full flex flex-col relative ">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space
+            gap-3">
+                <p className="sm:text-3xl text-2xl font-medium text-zinc-300 text-center font-generalsans ">Hi, I am Priyadeep <span className="waving-hand">🤚</span></p>
+                <p className="hero_tag text-gray_gradient">Weaving the web, one token at a time.</p>
+            </div>
+
+            <div className="w-full h-full absolute inset-0">
+                <Leva/>
+                <Canvas className="w-full h-full">
+                    <Suspense fallback={<CanvasLoader/>}>
+                        <PerspectiveCamera makeDefault position={[0,0,30]}/>
+                          <HackerRoom
+                              // scale={0.07}
+                              position={[x.positionX, x.positionY, x.positionZ]}
+                              rotation={[x.rotationX,x.rotationY,x.rotationZ ]}
+                              scale={[x.scale,x.scale,x.scale]}
+                          />
+
+                            <ambientLight intensity={1}/>
+                            <directionalLight position={[10,10,10]} intensity={0.5}/>
+                    </Suspense>
+                </Canvas>
+            </div>
+        </section>
+    )
+}
+export default Hero
