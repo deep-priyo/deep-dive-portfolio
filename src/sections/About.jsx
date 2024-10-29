2236271b18fadc4d7bfa41a803a0069625ec1262
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-10-29 23:17:17 +0530

    added several features to Bento-Grid

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,45 +1,105 @@
-import Globe from "react-globe.gl";
+import { useState } from 'react';
+import Globe from 'react-globe.gl';
 
+import Button from '../components/Button.jsx';
 
 const About = () => {
+    const [hasCopied, setHasCopied] = useState(false);
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText('priyadeepmullick@gmail.com');
+        setHasCopied(true);
+
+        setTimeout(() => {
+            setHasCopied(false);
+        }, 2000);
+    };
+
     return (
-        <section className="c-space my-20 ">
-            <div className="grid xl:grid-cols-3 xl:grid-rows-6  md:grids-cols-2 grid-cols-1 gap-5 h-full">
-                <div className="col-span-1 xl:row-span-3 ">
+        <section className="c-space my-20" id="about">
+            <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+                <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
-                        <img src="/assets/grid1.png" alt="grid-1" className= "w-full sm:h-[276px] h-fit object-contain"/>
+                        <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+
                         <div>
-                            <p className="grid-headtext">Hi, I'm Priyadeep</p>
-                            <p className="grid-subtext">With 3 years of focused learning, I've sharpened my frontend and backend skills, pushing myself daily to build seamless, impactful web experiences.</p>
+                            <p className="grid-headtext">Hi, I’m Priyadeep </p>
+                            <p className="grid-subtext">
+                                With  3 years of focused learning, I've sharpened my frontend and backend skills, pushing myself daily to build seamless, impactful web experiences.
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div className="col-span-1 xl:col-span-3 ">
-                    <div className="grid-container ">
-                        <img src="/assets/grid2.png" className="w-full sm:h-[276px] h-fit object-contain" alt="grid-2"/>
+
+                <div className="col-span-1 xl:row-span-3">
+                    <div className="grid-container">
+                        <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
 
                         <div>
                             <p className="grid-headtext">Tech Stack</p>
-                            <p className="grid-subtext ">II specialize in frontend technologies like React and Next.js, backend frameworks like Flask and Django, and databases such as SQL.</p>
+                            <p className="grid-subtext">
+                                I specialize in frontend technologies like React and Next.js, backend frameworks like Flask and Django, and databases such as SQL
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div className="col-span-1 xl:row-span-4">\
-                <div className="grid-container ">
-                    <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
-                        <Globe height={326} width={326} backgroundColor="rgba(0,0,0,0)" backgroundImageOpacity={0.5} showAtmosphere showGraticules
-                        globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
-                        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"/>
-                    </div>
-                    <div>
-                        <p className="grid-headtext">I'm open to remote work and relocation.</p>
-                        <p className="grid-subtext">I’m experienced in working remotely across time zones and am open to relocation, bringing flexibility and adaptability to any team.</p>
-                    </div>
 
+                <div className="col-span-1 xl:row-span-4">
+                    <div className="grid-container">
+                        <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+                            <Globe
+                                height={326}
+                                width={326}
+                                backgroundColor="rgba(0, 0, 0, 0)"
+                                backgroundImageOpacity={0.5}
+                                showAtmosphere
+                                showGraticules
+                                globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
+                                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                                labelsData={[{ lat: 22.5744, lng: 88.3629, text: 'Home', color: 'white', size: 15 }]}
+                            />
+                        </div>
+                        <div>
+                            <p className="grid-headtext">I’m remote-ready, flexible, and relocation-friendly.</p>
+                            <p className="grid-subtext">I’m open to working remotely across any time zone and am open to relocation, bringing flexibility and adaptability to any team.</p>
+                            <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+                        </div>
+                    </div>
                 </div>
+
+                <div className="xl:col-span-2 xl:row-span-3">
+                    <div className="grid-container">
+                        <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+
+                        <div>
+                            <p className="grid-headtext">Transforming ideas into code, one function at a time.</p>
+                            <p className="grid-subtext">
+                                I am driven by the thrill of solving complex problems and bringing ideas to life through code. Programming isn’t merely my career; it’s my true calling. It’s a relentless pursuit of excellence, where I push myself to write better code every day through hard work and dedication
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="xl:col-span-1 xl:row-span-2">
+                    <div className="grid-container">
+                        <img
+                            src="assets/grid4.png"
+                            alt="grid-4"
+                            className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+                        />
+
+                        <div className="space-y-2">
+                            <p className="grid-subtext text-center">Contact me</p>
+                            <div className="copy-container" onClick={handleCopy}>
+                                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
+                                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">priyadeepmullick@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
-    )
-}
-export default About
+    );
+};
+
+export default About;
