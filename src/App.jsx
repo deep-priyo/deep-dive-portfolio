95933a45907ac77839d170c4d4894a2d36629f0c
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-11-01 20:47:22 +0530

    changes in experience.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Navbar from "./sections/Navbar.jsx";
 import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
 import Projects from "./sections/Projects.jsx";
+import Clients from "./sections/Clients.jsx";
+import Footer from "./sections/Footer.jsx";
+import Contact from "./sections/Contact.jsx";
+import Experience from "./sections/Experience.jsx";
 
 const App = () => {
     return (
@@ -11,6 +15,10 @@ const App = () => {
             <Hero/>
             <About/>
             <Projects/>
+            <Clients/>
+            <Experience/>
+            <Contact/>
+            <Footer/>
         </main>
     )
 }
--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,56 +1,33 @@
-import { Suspense, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import {Canvas} from "@react-three/fiber";
+import {workExperiences} from "../constants/index.js";
 
-import Developer from '../components/Developer.jsx';
-import CanvasLoader from '../components/Loading.jsx';
-import { workExperiences } from '../constants/index.js';
 
-const WorkExperience = () => {
-    const [animationName, setAnimationName] = useState('idle');
-
+const Experience = () => {
     return (
-        <section className="c-space my-20" id="work">
-            <div className="w-full text-white-600">
-                <p className="head-text">My Work Experience</p>
-
+        <section className="c-space my-20 ">
+            <div className="w-full text-white-600 ">
+               <h3 className="head-text">My Work Experience</h3>
                 <div className="work-container">
                     <div className="work-canvas">
                         <Canvas>
-                            <ambientLight intensity={7} />
-                            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-                            <directionalLight position={[10, 10, 10]} intensity={1} />
-                            <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
 
-                            <Suspense fallback={<CanvasLoader />}>
-                                <Developer position-y={-3} scale={3} animationName={animationName} />
-                            </Suspense>
                         </Canvas>
                     </div>
-
                     <div className="work-content">
-                        <div className="sm:py-10 py-5 sm:px-5 px-2.5">
-                            {workExperiences.map((item, index) => (
-                                <div
-                                    key={index}
-                                    onClick={() => setAnimationName(item.animation.toLowerCase())}
-                                    onPointerOver={() => setAnimationName(item.animation.toLowerCase())}
-                                    onPointerOut={() => setAnimationName('idle')}
-                                    className="work-content_container group">
+                        <div className="sm:py-10 py-5 sm:px-5">
+                            {workExperiences.map(({id, icon,name,pos,duration,title,animation,}, index) => (
+                                <div key={id} className="work-content_container group">
                                     <div className="flex flex-col h-full justify-start items-center py-2">
                                         <div className="work-content_logo">
-                                            <img className="w-full h-full" src={item.icon} alt="" />
+                                            <img src={icon} alt="logo" className="w-full h-full"/>
                                         </div>
-
-                                        <div className="work-content_bar" />
+                                        <div className="work-content_bar"/>
                                     </div>
-
-                                    <div className="sm:p-5 px-2.5 py-5">
-                                        <p className="font-bold text-white-800">{item.name}</p>
-                                        <p className="text-sm mb-5">
-                                            {item.pos} -- <span>{item.duration}</span>
+                                    <div className="sm:p-5 px:2.5 py-5">
+                                        <p className="font-bold text-white-800">
+                                            {name}
                                         </p>
-                                        <p className="group-hover:text-white transition-all ease-in-out duration-500">{item.title}</p>
+                                        <p className="text-sm mb"></p>
                                     </div>
                                 </div>
                             ))}
@@ -59,7 +36,6 @@ const WorkExperience = () => {
                 </div>
             </div>
         </section>
-    );
-};
-
-export default WorkExperience;
+    )
+}
+export default Experience
