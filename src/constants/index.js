e06ffeff143f53583d57fd1221ba5668c289d7ba
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-11-01 20:47:22 +0530

    changes in experience.jsx

--- src/constants/index.js
+++ src/constants/index.js
@@ -261,7 +261,7 @@ export const workExperiences = [
     {
         id: 1,
         name: 'Framer',
-        pos: 'Lead Web Developer',
+        pos: 'Lead Web Developer.jsx',
         duration: '2022 - Present',
         title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
         icon: '/assets/framer.svg',
@@ -270,7 +270,7 @@ export const workExperiences = [
     {
         id: 2,
         name: 'Figma',
-        pos: 'Web Developer',
+        pos: 'Web Developer.jsx',
         duration: '2020 - 2022',
         title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
         icon: '/assets/figma.svg',
@@ -279,7 +279,7 @@ export const workExperiences = [
     {
         id: 3,
         name: 'Notion',
-        pos: 'Junior Web Developer',
+        pos: 'Junior Web Developer.jsx',
         duration: '2019 - 2020',
         title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
         icon: '/assets/notion.svg',
