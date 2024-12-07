a376cf74e55f0ead1fe5e0a83fd77f71daac98e7
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-07 23:23:55 +0530

    styling changes create skill section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -22,7 +22,7 @@ const Projects = () => {
 
     return (
         <section id='work' className="c-space my-20">
-            <p className="head-text ">My Work</p>
+            <p className="head-text ">Explore My Work</p>
             <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
                 <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
                     <div className="absolute top-0 right-0">
@@ -33,8 +33,9 @@ const Projects = () => {
                     </div>
                     <div className="flex flex-col gap-5 text-white-600 my-5">
                         <p className="text-white text-2xl font-semibold animatedText">{currentProject.title}</p>
-                        <p className="animatedText">{currentProject.desc}</p>
-                        <p className="animatedText">{currentProject.subdesc}</p>
+
+                        <p className="animatedText text-justify">{currentProject.desc}</p>
+                        <p className="animatedText text-justify"><span className='text-zinc-700 font-semibold tracking-tight capitalize'>Skills I gained: </span>{currentProject.skill}</p>
                     </div>
                     <div className="flex items-center justify-between flex-wrap gap-5">
                         <div className="flex items-center gap-3">
