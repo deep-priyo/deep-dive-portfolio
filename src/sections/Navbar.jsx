ce697b2d84a221f0be9e126f01ead3ce464a6815
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-15 00:03:08 +0530

    minor changes

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
                 </div>
             </div>
             <div className={`nav-sidebar ${isOpen? 'max-h-screen':'max-h-0'}`}>
-                <nav className="p-5 ">
+                <nav className="p-5 p-5 ">
                     <NavItems/>
                 </nav>
             </div>
